40cfa61dbca0e47e597f3fd68a61a00f31b0effc
fabiopulvirenti <fpulvirenti002@gmail.com>
2023-11-17 16:43:29 +0000

    Add Tests

--- src/test/java/com/sparta/fp/atm/AppTest.java
+++ src/test/java/com/sparta/fp/atm/AppTest.java
@@ -12,11 +12,43 @@ public class AppTest
 {
     /**
      * Rigorous Test :-)
-     */
-    @Test
-    public void test()
-    {
+     */@Test
+    public void testWithdrawalWithSufficientNotes() {
+        ATM atm = new ATM();
+        int initialNoteOf50 = atm.getNoteOf50();
+        int initialNoteOf20 = atm.getNoteOf20();
+        int initialNoteOf10 = atm.getNoteOf10();
 
+        int withdrawalAmount = 70;
+        atm.withdrawValue(withdrawalAmount);
 
+        int expectedNoteOf50 = initialNoteOf50 - 1;
+        int expectedNoteOf20 = initialNoteOf20 - 1;
+        int expectedNoteOf10 = initialNoteOf10;
+
+        Assertions.assertEquals(expectedNoteOf50, atm.getNoteOf50());
+        Assertions.assertEquals(expectedNoteOf20, atm.getNoteOf20());
+        Assertions.assertEquals(expectedNoteOf10, atm.getNoteOf10());
     }
+
+         @Test
+    public void testWithdrawalWithSufficientNotes_Withdraw80() {
+        ATM atm = new ATM();
+        int initialNoteOf50 = atm.getNoteOf50();
+        int initialNoteOf20 = atm.getNoteOf20();
+        int initialNoteOf10 = atm.getNoteOf10();
+
+        int withdrawalAmount = 80;
+        atm.withdrawValue(withdrawalAmount);
+
+        int expectedNoteOf50 = initialNoteOf50 - 1;
+        int expectedNoteOf20 = initialNoteOf20 - 1;
+        int expectedNoteOf10 = initialNoteOf10 -1;
+
+        Assertions.assertEquals(expectedNoteOf50, atm.getNoteOf50());
+        Assertions.assertEquals(expectedNoteOf20, atm.getNoteOf20());
+        Assertions.assertEquals(expectedNoteOf10, atm.getNoteOf10());
+    }
+
+
 }
